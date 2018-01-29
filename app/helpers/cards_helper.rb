e66ae7996491707c8d1942e659747e2966c20275
Dan DeFelippi <dan@driverdan.com>
2018-01-28 18:36:01 -0600

    Improved views, add i18n time formatting improvements

--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -1,2 +1,9 @@
 module CardsHelper
+  ##
+  # Check if a card has access to a door
+  ##
+  def has_access?(access_log, access_groups)
+    doors = access_groups.map(&:doors).flatten.uniq
+    doors.map(&:name).include? access_log.door
+  end
 end
