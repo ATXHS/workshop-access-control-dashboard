c44af1556df53d217a616ba05bf2abbf5ac14e0b
Dan DeFelippi <dan@driverdan.com>
2018-02-02 17:19:27 -0600

    Style and design improvements

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  ##
+  # Generate a menu list item, checking for the active path
+  ##
+  def menu_item(text, path)
+    item_class = current_page?(path) ? 'is-active' : ''
+
+    "<li class=#{item_class}>#{link_to text, path}</li>".html_safe
+  end
 end
